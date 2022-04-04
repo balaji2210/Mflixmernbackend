505e39611c45d4956136d98e46f33ee1aa458874
balaji2210 <63396862+balaji2210@users.noreply.github.com>
2022-04-04 17:33:56 +0530

    banner errors updated

--- controllers/banner.js
+++ controllers/banner.js
@@ -3,121 +3,132 @@ const BigPromise = require("../middlewares/Bigpromise");
 
 const cloudinary = require("cloudinary").v2;
 exports.createBanner = BigPromise(async (req, res, next) => {
-  let image = {};
-  let result;
-  if (req.files) {
-    result = await cloudinary.uploader.upload(req.files.photo.tempFilePath, {
-      folder: "banner",
-    });
-  }
-  image.public_id = result.public_id;
-  image.secure_url = result.secure_url;
+	let image = {};
+	let result;
+	if (req.files) {
+		result = await cloudinary.uploader.upload(
+			req.files.photo.tempFilePath,
+			{
+				folder: "banner",
+			}
+		);
+	}
+	image.public_id = result.public_id;
+	image.secure_url = result.secure_url;
 
-  req.body.photo = image;
-  req.body.user = req.user._id;
+	req.body.photo = image;
+	req.body.user = req.user._id;
 
-  const banner = await Banner.create(req.body);
+	const banner = await Banner.create(req.body);
 
-  res.status(200).json({
-    success: true,
-    banner,
-  });
+	res.status(200).json({
+		success: true,
+		banner,
+	});
 });
 
 exports.getABanner = BigPromise(async (req, res, next) => {
-  const banner = await Banner.findById(req.params.id);
+	const banner = await Banner.findById(req.params.id);
 
-  if (!banner) {
-    return res.status(400).json({
-      success: true,
-      message: "No Banner found with Id",
-    });
-  }
+	if (!banner) {
+		return res.status(400).json({
+			success: true,
+			message: "No Banner found with Id",
+		});
+	}
 
-  return res.status(200).json({
-    success: true,
-    banner,
-  });
+	return res.status(200).json({
+		success: true,
+		banner,
+	});
 });
 
 exports.getBanners = BigPromise(async (req, res, next) => {
-  const banners = await Banner.find();
+	const banners = await Banner.find();
 
-  if (!banners) {
-    return res.status(400).json({
-      success: true,
-      message: "No Banner found with Id",
-    });
-  }
+	if (!banners) {
+		return res.status(400).json({
+			success: true,
+			message: "No Banner found with Id",
+		});
+	}
 
-  return res.status(200).json({
-    success: true,
-    banners,
-  });
+	return res.status(200).json({
+		success: true,
+		banners,
+	});
 });
 
 exports.updateBanner = BigPromise(async (req, res, next) => {
-  let banner = await Banner.findById(req.params.id);
+	let banner = await Banner.findById(req.params.id);
 
-  if (!banner) {
-    return res.status(400).json({
-      success: true,
-      message: "No Banner found with Id",
-    });
-  }
+	if (!banner) {
+		return res.status(400).json({
+			success: true,
+			message: "No Banner found with Id",
+		});
+	}
 
-  let image = {};
-  let result;
+	// let image = {};
+	// let result;
 
-  if (req.files) {
-    await cloudinary.uploader.destroy(banner.image.public_id);
+	// if (req.files) {
+	//   await cloudinary.uploader.destroy(banner.image.public_id);
 
-    result = await cloudinary.uploader.upload(req.files.photo.tempFilePath, {
-      folder: "banner",
-    });
+	//   result = await cloudinary.uploader.upload(req.files.photo.tempFilePath, {
+	//     folder: "banner",
+	//   });
 
-    image.public_id = result.public_id;
-    image.secure_url = result.secure_url;
-    req.body.photo = image;
-  }
+	//   image.public_id = result.public_id;
+	//   image.secure_url = result.secure_url;
+	//   req.body.photo = image;
+	// }
 
-  banner = await Banner.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-    useFindAndModify: false,
-  });
+	// banner = await Banner.findByIdAndUpdate(req.params.id, req.body, {
+	//   new: true,
+	//   runValidators: true,
+	//   useFindAndModify: false,
+	// });
 
-  return res.status(200).json({
-    success: true,
-    banner,
-  });
+	if (banner.is_featured === "true") {
+		banner.is_featured = "false";
+	} else {
+		banner.is_featured = "true";
+	}
+
+	await banner.save();
+
+	return res.status(200).json({
+		success: true,
+		banner,
+	});
 });
 
 exports.deleteBanner = BigPromise(async (req, res, next) => {
-  const banner = await Banner.findById(req.query.id);
+	const banner = await Banner.findById(req.query.id);
 
-  await cloudinary.uploader.destroy(banner.photo.public_id);
+	await cloudinary.uploader.destroy(banner.photo.public_id);
 
-  await Banner.findByIdAndDelete(req.query.id);
+	await Banner.findByIdAndDelete(req.query.id);
 
-  return res.status(200).json({
-    success: true,
-    message: "Banner Deleted",
-  });
+	return res.status(200).json({
+		success: true,
+		message: "Banner Deleted",
+	});
 });
 
 exports.trending = BigPromise(async (req, res, next) => {
-  const banners = await Banner.find({ is_featured: true });
+	const banners = await Banner.find({ is_featured: "true" });
 
-  if (!banners) {
-    return res.status(400).json({
-      success: true,
-      message: "No Banner found with Id",
-    });
-  }
+	if (!banners) {
+		return res.status(400).json({
+			success: true,
+			message: "No Banner found with Id",
+		});
+	}
 
-  return res.status(200).json({
-    success: true,
-    banners,
-  });
+	return res.status(200).json({
+		success: true,
+		banners,
+	});
 });
--- models/Banner.js
+++ models/Banner.js
@@ -1,28 +1,28 @@
 const mongoose = require("mongoose");
 
 const bannerSchema = new mongoose.Schema(
-  {
-    photo: {
-      public_id: {
-        type: String,
-        required: true,
-      },
-      secure_url: {
-        type: String,
-        required: true,
-      },
-    },
-    is_featured: {
-      type: Boolean,
-      default: false,
-    },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: "User",
-      required: true,
-    },
-  },
-  { timestamps: true }
+	{
+		photo: {
+			public_id: {
+				type: String,
+				required: true,
+			},
+			secure_url: {
+				type: String,
+				required: true,
+			},
+		},
+		is_featured: {
+			type: String,
+			default: "false",
+		},
+		user: {
+			type: mongoose.Schema.ObjectId,
+			ref: "User",
+			required: true,
+		},
+	},
+	{ timestamps: true }
 );
 
 module.exports = mongoose.model("Banner", bannerSchema);
